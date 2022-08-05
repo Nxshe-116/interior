c0f4222c19ebefa6da364dc989484a66e23abe8d
Nashe Chagumaira <77992898+Nxshe-116@users.noreply.github.com>
2022-08-05 07:53:51 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,71 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">`
+    <style>
+    
+        *{
+
+            font-family: 'Roboto Mono', monospace;
+        }
+        [class*="col"]{
+            padding-top: 0px;
+            background-color: #fefefe;
+            color: #000;    
+        }
+        
+        .text-center {
+  text-align: center;
+}
+
+html,body {
+  height: 100%;
+  width: 100%;
+}
+
+.wrapper {
+  height: 75%;
+  width: 100%;
+  line-height: 1.5;
+  word-spacing: 4px;
+  letter-spacing: 1px;
+}
+
+.fixed-bg {
+  opacity: 95%;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  height: 100%; 
+  width: 100%;
+  color: white;
+  text-align: center;
+  display: table;
+}
+
+.fixed-bg h1 {
+
+  display: table-cell;
+  vertical-align: middle;
+}
+
+
+.bg-1 {
+  background-image: url('assets\\mm1.jpg');
+}
+
+
+    </style>
+</head>
---
+++ index.php
@@ -0,0 +1,161 @@
+<?php include('header.php'); ?>
+<?php include('nav.php'); ?>
+
+<body>
+
+  <div style="padding-top:40px ;" class=" container fluid ">
+    <div class="row">
+      <div class="col center-block text-center">
+        <h6>This is</h6>
+        <h1 class="display-4">Studio By Interior</h1>
+      </div>
+    </div>
+  </div>
+
+  <div style="padding-top:100px;" class=" container fluid ">
+
+    <div class="row">
+      <div class="col-md-6">
+        <img src="assets\mm3.jpg" class="img-fluid" alt="...">
+      </div>
+      <div style="padding-top:100px;" class="col-md-6 text-center align-items-center">
+        <h1 class="display-6 p-2">Feel the style</h1>
+        <p class="p-2">We create ergonomic spaces with individual design</p>
+        <button type="button" class="btn btn-dark">Get Started</button>
+      </div>
+    </div>
+
+  </div>
+
+  <div style="padding-bottom:150px; padding-top:100px;" class=" text-center ">
+    <h1 class=" p-4 display-4">Service Packages</h1>
+
+    <div class="card-group">
+      <div class="card">
+        <img src="assets\sketch3.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Sketch Design Project</h5>
+          <p class="card-text">
+          <ul class="list-unstyled">
+            <li>Furniture planning solutions</li>
+            <li>General concept stylistic collage</li>
+            <li>Minimum set of working drawings for a contractor</li>
+          </ul>
+          </p>
+
+        </div>
+      </div>
+      <div class="card">
+        <img src="assets\sketch2.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Full Design Project</h5>
+          <p class="card-text">
+          <ul class="list-unstyled">
+            <li>Furniture planning solutions</li>
+            <li>General concept stylistic collage</li>
+            <li>Minimum set of working drawings for a contractor</li>
+            <li>Photorealistic visualization of the room.</li>
+          </ul>
+          </p>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <div style="padding-bottom:250px" class=" wrapper">
+
+    <div class="fixed-bg bg-1">
+
+      <h1 class="display-6">Choose your own design</h1>
+    </div>
+
+    <div class="container  ">
+    <div style="padding-top:40px ;" class=" container fluid ">
+    <div class="row">
+      <div class="col center-block text-center">
+        <h6 style="padding-bottom: 40px;">We love when "less words mean more work" and create a design you want to look at forever</h6>
+        <button type="button" class="btn btn-dark">Get Started</button>
+      </div>
+    </div>
+  </div>
+    </div>
+  </div>
+
+    <div style="padding-bottom:120px;" class="container fluid text-center">
+
+    <h1 style="padding-bottom:50px; padding-top:50px;"class="display-5"> Testimonials</h1>
+
+
+    <section>
+  <div class="row text-center">
+    <div class="col-md-12">
+      <!-- Carousel wrapper -->
+      <div id="carouselBasicExample" class="carousel slide carousel-dark" data-mdb-ride="carousel">
+        <!-- Inner -->
+        <div class="carousel-inner">
+          <!-- Single item -->
+          <div class="carousel-item active">
+            <p class="lead font-italic mx-4 mx-md-5">
+              "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet
+              numquam iure provident voluptate esse quasi, voluptas nostrum quisquam!"
+            </p>
+            <div class="mt-5 mb-4">
+              <img src="assets\team\Keith.jpg"
+                class="rounded-circle img-fluid shadow-1-strong" alt="..." width="100"
+                height="100" />
+            </div>
+            <p class="text-muted mb-0">- Keith Mupinda</p>
+          </div>
+
+          <!-- Single item -->
+          <div class="carousel-item">
+            <p class="lead font-italic mx-4 mx-md-5">
+              "Neque cupiditate assumenda in maiores repudiandae mollitia adipisci maiores
+              repudiandae mollitia consectetur adipisicing architecto elit sed adipiscing
+              elit."
+            </p>
+            <div class="mt-5 mb-4">
+              <img src="assets\team\Nashe.jpg"
+                class="rounded-circle img-fluid shadow-1-strong" alt="..." width="100"
+                height="100" />
+            </div>
+            <p class="text-muted mb-0">- Nashe Medan</p>
+          </div>
+
+          <!-- Single item -->
+          <div class="carousel-item">
+            <p class="lead font-italic mx-4 mx-md-5">
+              "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+              dolore eu fugiat nulla pariatur est laborum neque cupiditate assumenda in
+              maiores."
+            </p>
+            <div class="mt-5 mb-4">
+              <img src="assets\team\Obey.jpg"
+                class="rounded-circle img-fluid shadow-1-strong" alt="..." width="100"
+                height="100" />
+            </div>
+            <p class="text-muted mb-0">- Obey Mandipa</p>
+          </div>
+        </div>
+        <!-- Inner -->
+
+        <!-- Controls -->
+        <button class="carousel-control-prev" type="button" data-mdb-target="#carouselBasicExample"
+          data-mdb-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-mdb-target="#carouselBasicExample"
+          data-mdb-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      <!-- Carousel wrapper -->
+    </div>
+  </div>
+</section>
+    </div>
+
+  <?php include('footer.php'); ?>
